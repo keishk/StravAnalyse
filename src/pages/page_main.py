a655407daa15ec89c71bc46f963fcba7c082bf79
kkohara <keishi.kohara@hal24k-water.com>
2021-12-28 12:22:08 +0000

    enabled token login, fixed lack of heartrate monitor

--- src/pages/page_main.py
+++ src/pages/page_main.py
@@ -8,7 +8,7 @@ from src.strava_api import get_activity, get_data
 
 
 def run_page_main():
-    list_methods = ["login"]
+    list_methods = ["login", "token"]
     list_users = ["demo", "k1", "c1"]
     list_sport = ["run", "swim", "ride"]
     list_metrics = [
@@ -24,15 +24,38 @@ def run_page_main():
         if login_method == "login":
             user = st.selectbox("User", list_users)
             password = st.text_input("Password", type="password")
+            client_id = None
+            client_secret = None
+            refresh_token = None
+            submit_token = None
 
         elif login_method == "token":
-            strava_client_id = st.text_input("Client ID")
-            strava_client_secret = st.text_input("Client secret")
-            strava_refresh_token = st.text_input("Refresh token")
+            with st.form("token_form"):
+                client_id = st.text_input("Client ID")
+                client_secret = st.text_input("Client secret")
+                refresh_token = st.text_input("Refresh token")
+                submit_token = st.form_submit_button("Submit")
+                user = "None"
+                password = "None"
 
-    if (user == "demo") | ((user != "demo") & (password == st.secrets[user]["pass"])):
+    if any(
+        (
+            user == "demo",
+            (login_method == "login")
+            & (user != "demo")
+            & (password == st.secrets[user]["pass"]),
+            submit_token,
+        )
+    ):
+        my_dataset = get_data(
+            app=True,
+            login_method=login_method,
+            user=user,
+            client_id=client_id,
+            client_secret=client_secret,
+            refresh_token=refresh_token,
+        )
         sport = st.select_slider("", list_sport)
-        my_dataset = get_data(app=True, user=user)
         _, runs, rides, swims = process_data(my_dataset, user=user)
         if sport == "run":
             df = runs
@@ -53,41 +76,47 @@ def run_page_main():
         st.header("")
 
         st.header("Activity Heatmap")
-        c2_1, _1, c2_3, _2, = st.columns([1, 2, 1, 2])
+        c2_1, c2_2, = st.columns(2)
         list_years = ["2018", "2019", "2020", "2021"]
         with c2_1:
-            year_1 = st.selectbox("", list_years)
+            year_1 = st.selectbox("", list_years, len(list_years) - 2)
             fig_heatmap_1 = plot_heatmap(df, year_1)
             folium_static(fig_heatmap_1, 500)
-        with c2_3:
-            year_2 = st.selectbox("", list_years, 1)
+        with c2_2:
+            year_2 = st.selectbox("", list_years, len(list_years) - 1)
             fig_heatmap_2 = plot_heatmap(df, year_2)
             folium_static(fig_heatmap_2, 500)
-        _1.text("")
-        _2.text("")
+
         st.header("")
 
         st.header("Activity Performance")
 
-        df_act_list = df.copy().reset_index(drop=True)
+        df_display = df.copy().reset_index(drop=True)
         cols_float = [
             "distance (km)",
             "moving_time (min)",
             "total_elev_gain (m)",
             "avg_speed (km/h)",
+        ]
+        cols_device = [
             "average_cadence",
             "average_heartrate",
             "max_heartrate",
         ]
-        df_act_list = df_act_list[["date", "name"] + cols_float + ["id"]]
-        df_act_list.loc[:, "date"] = df_act_list["date"].dt.strftime("%d-%m-%Y")
-        # st.dataframe(df_act_list.style.format(subset=cols_float, formatter="{:.2f}"))
+        if cols_device[0] in df_display:
+            df_display = df_display[
+                ["date", "name"] + cols_float + cols_device + ["id"]
+            ]
+        else:
+            df_display = df_display[["date", "name"] + cols_float + ["id"]]
 
-        gb = GridOptionsBuilder.from_dataframe(df_act_list)
+        df_display.loc[:, "date"] = df_display["date"].dt.strftime("%m-%d-%Y")
+
+        gb = GridOptionsBuilder.from_dataframe(df_display)
         gb.configure_pagination(paginationAutoPageSize=True)
         gb.configure_selection("single")
         grid_response = AgGrid(
-            df_act_list,
+            df_display,
             gridOptions=gb.build(),
             height=305,
             update_mode=GridUpdateMode.SELECTION_CHANGED,
@@ -124,3 +153,4 @@ def run_page_main():
 
     else:
         st.warning("Enter password.")
+
--- src/strava_api.py
+++ src/strava_api.py
@@ -8,19 +8,31 @@ import streamlit as st
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def get_data(app, user):
+def get_data(
+    app, login_method, user, client_id=None, client_secret=None, refresh_token=None
+):
     auth_url = "https://www.strava.com/oauth/token"
     activites_url = "https://www.strava.com/api/v3/athlete/activities"
 
     # POST request, so needs payload
     if app:
-        payload = {
-            "client_id": st.secrets[user]["strava_client_id"],
-            "client_secret": st.secrets[user]["strava_client_secret"],
-            "refresh_token": st.secrets[user]["strava_refresh_token"],
-            "grant_type": "refresh_token",
-            "f": "json",
-        }
+        if login_method == "login":
+            payload = {
+                "client_id": st.secrets[user]["strava_client_id"],
+                "client_secret": st.secrets[user]["strava_client_secret"],
+                "refresh_token": st.secrets[user]["strava_refresh_token"],
+                "grant_type": "refresh_token",
+                "f": "json",
+            }
+        else:
+            payload = {
+                "client_id": client_id,
+                "client_secret": client_secret,
+                "refresh_token": refresh_token,
+                "grant_type": "refresh_token",
+                "f": "json",
+            }
+
     else:
         from config import strava_client_id, strava_client_secret, strava_refresh_token
 
